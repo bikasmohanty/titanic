49f84933aea1e47f6d73767d4e8060c852956baa
Mohanty, Bikas <bikas.mohanty@accenture.com>
2019-11-19 17:10:18 +0530

    my first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+app = Flask(__name__)
+# Code to load ML model
+@app.route('/')
+def home():    
+     return render_template(“filename.html”)
+@app.route('/ml-model', methods=['POST'])
+def run_model():
+     # Code to use the trained model to make real time predictions
+     return render_template(“filename.html”, result)
+if __name__ == '__main__':
+     app.run()
